d36eb7ce07b6af3797e67ee3537e42d5cdcaf405
shilpa <shilpa07udupi@gmail.com>
2020-09-08 11:33:16 +0530

    adding last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,17 @@
 echo welcome to user registration problem !
 
-read -p "Enter valid lirst name " lName
+read -p "Enter valid first name " fName
+
+pattern="^([A-Z])[A-Za-z]{2,}$"
+
+if [[ $fName =~ $pattern ]]
+then
+        echo valid
+else
+        echo not valid
+fi
+
+read -p "Enter valid last name " lName
 
 pattern="^([A-Z])[A-Za-z]{2,}$"
 
