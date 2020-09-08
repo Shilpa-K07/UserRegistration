a4a79874c1c7c5f7424b7e3dddb0058308b96be4
shilpa <shilpa07udupi@gmail.com>
2020-09-08 20:55:17 +0530

    validating email

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,7 +2,7 @@ echo welcome to user registration problem !
 
 read -p "Enter valid email " email
 
-pattern="^([0-9A-Za-z])+([.-+_][0-9A-Za-z]+)*@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*$"
+pattern="^([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*$"
 
 if [[ $email =~ $pattern ]]
 then
