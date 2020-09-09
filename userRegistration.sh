30c8e43c1f345b17f39ba3e9c305b30bf8b12539
shilpa <shilpa07udupi@gmail.com>
2020-09-09 12:33:00 +0530

    Adding first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,7 +2,7 @@ echo welcome to user registration problem !
 
 read -p "Enter valid first name " fName
 
-pattern="^([A-Z])[A-Za-z]{2,}$"
+pattern="^[[:upper:]][A-Za-z]{2,}$"
 
 if [[ $fName =~ $pattern ]]
 then
